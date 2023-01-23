8478460d0690df30a6fb8d3e868ee9a043fe5c7f
Pusaka Manggala <pusakamanggala@gmail.com>
2023-01-23 18:31:43 +0700

    add : database

---
+++ Database/vaksinasi.sql
@@ -0,0 +1,139 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 23, 2023 at 12:30 PM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 8.0.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `vaksinasi`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `login`
+--
+
+CREATE TABLE `login` (
+  `id` int(20) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `login`
+--
+
+INSERT INTO `login` (`id`, `username`, `password`) VALUES
+(4, 'admin', 'admin');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `nik` varchar(20) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `alamat` text NOT NULL,
+  `no_hp` varchar(20) NOT NULL,
+  `status_sertif` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`nik`, `nama`, `alamat`, `no_hp`, `status_sertif`) VALUES
+('3626307959869261', 'John Doe', 'Jalan kota menuju kampung', '083131827492', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `vaksin`
+--
+
+CREATE TABLE `vaksin` (
+  `id` int(20) NOT NULL,
+  `nik` varchar(20) NOT NULL,
+  `type` varchar(10) NOT NULL,
+  `date` varchar(10) NOT NULL,
+  `batch` int(5) NOT NULL,
+  `locations` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `vaksin`
+--
+
+INSERT INTO `vaksin` (`id`, `nik`, `type`, `date`, `batch`, `locations`) VALUES
+(19, '3626307959869261', 'Vaksin 2', '2023-02-17', 3, 'Telkom University');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `login`
+--
+ALTER TABLE `login`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`nik`);
+
+--
+-- Indexes for table `vaksin`
+--
+ALTER TABLE `vaksin`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `nik_2` (`nik`),
+  ADD KEY `nik` (`nik`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `login`
+--
+ALTER TABLE `login`
+  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `vaksin`
+--
+ALTER TABLE `vaksin`
+  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `vaksin`
+--
+ALTER TABLE `vaksin`
+  ADD CONSTRAINT `vaksin_ibfk_1` FOREIGN KEY (`nik`) REFERENCES `users` (`nik`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
