c227308aff90fd6b4cf4c975539c9a256c01ed93
Pusaka Manggala <pusakamanggala@gmail.com>
2023-01-23 18:02:09 +0700

    add : page to create certificate

---
+++ buat_sertif.php
@@ -0,0 +1,28 @@
+<?php
+
+include_once("koneksi.php");
+
+$varnik = $_POST['nik'];
+$intbatch = $_POST['batch'];
+$vardate = $_POST['date'];
+$varlocation = $_POST['location'];
+$vartype = $_POST['type'];
+
+$simpan = "INSERT into vaksin values ('', '$varnik', '$vartype', '$vardate', '$intbatch', '$varlocation')";
+
+
+
+if ($koneksi->query($simpan)) {
+    $updateStatus = mysqli_query($koneksi, "UPDATE users SET status_sertif= 1 WHERE nik='$nik' ");
+    echo "<script> 
+                    alert('SUBMIT BERHASIL'); 
+                    </script>";
+                    header("Location:create_sertif.php");
+} else {
+    echo "<script> 
+                    alert('NIK TIDAK DITEMUKAN ATAU SERTIFIKAT SUDAH DIBUAT'); 
+                    </script>";
+
+};
+
+?>
